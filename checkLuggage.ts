342b61635c29e4d50e621a87856b35d612eb021b
Sae Nakatani <nakatanisae0218@gmail.com>
2026-08-17 15:40:08 -0700

    Make carry-on luggage class

--- checkLuggage.ts
+++ checkLuggage.ts
@@ -78,3 +78,61 @@ class RegularLuggage extends Luggage {
     }
 
 }
+
+class FragileLuggage extends Luggage {
+
+    private insurance: number;
+
+    constructor(weight: number, description: string, priority: Priority, insurance: number) {
+        super(weight, description, priority)
+        this.insurance = insurance;
+    }
+
+    getInsuranceValue(): number {
+        return this.insurance
+    }
+
+    setInsuranceValue(value: number): void {
+        this.insurance = value;
+    }
+
+    getPrice(): number {
+        switch (this.priority) {
+            case Priority.Normal:
+                return this.insurance;
+            case Priority.Priority:
+                return this.insurance + this.fee * 5;
+            case Priority.Urgent:
+                return this.insurance + this.fee * 10;
+            default:
+                return 0;
+        }
+    }
+
+    toString(): string {
+        return `${this.description} (Fragile) - Insurance: ${this.insurance}`
+    }
+
+}
+
+class CarryOnLuggage extends Luggage {
+    constructor(weight: number, description: string, priority: Priority) {
+        super(weight, description, priority)
+    }
+
+    getPrice(): number {
+        const extraWeightCarry = this.weight - 5
+
+        if (this.weight <= 5) {
+            return 0
+        } else {
+            return this.fee * 3 * extraWeightCarry
+        };
+
+    }
+
+    toString(): string {
+        return `${this.description} (Carry-on)`
+    }
+
+}
