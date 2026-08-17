4e452df5ec7d6fbbaf132b21a4c2b5dae783d915
Sae Nakatani <nakatanisae0218@gmail.com>
2026-08-17 15:17:40 -0700

    Make Regular luggage class

--- checkLuggage.ts
+++ checkLuggage.ts
@@ -1,49 +1,80 @@
-enum Priority{
+enum Priority {
     Normal = "Normal",
     Priority = "Priority",
     Urgent = "Urgent"
 }
-  
 
 
-abstract class Luggage{
-        protected weight: number;
-        protected description: string;
-        protected priority: Priority;
-        protected readonly fee: number = 5.20;
-    
-    
-    constructor(weight: number, description: string, priority: Priority){
+
+abstract class Luggage {
+    protected weight: number;
+    protected description: string;
+    protected priority: Priority;
+    protected readonly fee: number = 5.20;
+
+
+    constructor(weight: number, description: string, priority: Priority) {
         this.weight = weight;
         this.description = description;
         this.priority = priority;
     }
 
-    getWeight(): number{
+    getWeight(): number {
         return this.weight;
     }
 
-    setWeight(weight: number){
+    setWeight(weight: number) {
         this.weight = weight;
     }
 
-    getDescription(): string{
+    getDescription(): string {
         return this.description;
     }
 
-    getPriority():Priority{
+    getPriority(): Priority {
         return this.priority
     }
 
     abstract getPrice(): number;
     abstract toString(): string;
 
-    getInsuranceValue(): number{
+    getInsuranceValue(): number {
         return 0;
     }
 
-    setInsuranceValue(value: number): string{
+    setInsuranceValue(value: number): string {
         return "Insurance is only applicable to fragile luggage.";
+    }
+
+}
+
+class RegularLuggage extends Luggage {
+
+    constructor(weight: number, description: string, priority: Priority) {
+        super(weight, description, priority);
+    }
+
+    getPrice(): number {
+        if (this.weight <= 23) {
+            return 0
+        };
+
+        const extraWeight = this.weight - 23
+
+        switch (this.priority) {
+            case Priority.Normal:
+                return this.fee * extraWeight;
+            case Priority.Priority:
+                return this.fee * 5 * extraWeight;
+            case Priority.Urgent:
+                return this.fee * 10 * extraWeight;
+            default:
+                return 0;
+        }
+    }
+
+    toString(): string {
+        return `${this.description} (Regular)`
     }
 
 }
